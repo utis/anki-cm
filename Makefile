a5451f415cada3cebd1c7af126d3d1adf1431de7
Oliver Utis <oliver.utis@gmail.com>
2024-08-04 15:47:28 +0200

    Prefixing all files with ankicm-; more sophisticated Makefile; creating chaos and cleaning it up.

--- Makefile
+++ Makefile
@@ -1,56 +1,74 @@
-srcdir  := src
-distdir := dist
-nodedir := node_modules
-cmdir := cm-chessboard
-ankidir := ~/.local/share/Anki2/Egoge/collection.media/ 
-target-files := _anki-cm.js _standard.svg _staunty.svg _chessboard.css
-targets := $(target-files:%=$(distdir)/%)
-extra-targets := $(distdir)/index.html
-chessdir := chess.js/src
-boarddir := cm-chessboard/src
+INSTALLDIR := ~/.local/share/Anki2/Egoge/collection.media/ 
 
-# .PHONY: test
-# test:
-# 	echo $(targets)
+SRCDIR  := src
+DISTDIR := dist
+CMDIR := cm-chessboard
+CHESSDIR := chess.js
+NODEDIR := node_modules
+
+TARGET_FILES := _ankicm.js _ankicm-standard.svg _ankicm-staunty.svg _ankicm.css
+# TARGET_FILES := _ankicm.js
+TARGETS := $(TARGET_FILES:%=$(DISTDIR)/%)
+EXTRA_TARGETS := $(DISTDIR)/index.html
+
+# cm-chessboard's extensions:
+EXTS := markers accessibility promotion-dialog
+
+exts_js := $(foreach e,$(EXTS),$(wildcard $(CMDIR)/src/extensions/$e/*.js))
+exts_css := $(foreach e,$(EXTS),$(wildcard $(CMDIR)/assets/extensions/$e/*.css))
+exts_svg := $(foreach e,$(EXTS),$(wildcard $(CMDIR)/assets/extensions/$e/*.svg))
+exts_svg_targets := $(EXTS:%=$(DISTDIR)/ankicm-%.svg)
+
+.PHONY: test
+
+vpath %.js $(SRCDIR) $(CMDIR)/src/ $(dir $(exts_js)) $(CHESSDIR)/src
+vpath %.css $(SRCDIR)/css $(CMDIR)/assets $(dir $(exts_css))
+vpath %.svg $(SRCDIR)/pieces $(CMDIR)/assets/pieces $(dir $(exts_svg))
+vpath % $(SRCDIR)
 
 .DELETE_ON_ERROR:
 
 .PHONY: all
 
-all: esbuild $(targets) $(extra-targets)
+#all: esbuild $(TARGETS) $(EXTRA_TARGETS)
+all: $(TARGETS) $(EXTRA_TARGETS)
 
-$(distdir)/_anki-cm.js: $(srcdir)/anki-cm.js $(boarddir)/Chessboard.js $(chessdir)/chess.js
-	./$(nodedir)/.bin/esbuild $< --bundle --outfile=$@
+# Main JS target
+$(DISTDIR)/_ankicm.js: ankicm.js
+	./$(NODEDIR)/.bin/esbuild $^ --bundle --minify --outfile=$@
 
-$(distdir)/_standard.svg: $(cmdir)/assets/pieces/standard.svg
+$(DISTDIR)/_ankicm-%.svg: %.svg
 	cp -f $< $@
 
-$(distdir)/_staunty.svg: $(cmdir)/assets/pieces/staunty.svg
+# $(distdir)/_ankicm-standard.svg: standard.svg
+# 	cp -f $< $@
+
+# $(distdir)/_ankicm-staunty.svg: staunty.svg
+# 	cp -f $< $@
+
+$(DISTDIR)/index.html: $(SRCDIR)/index.html
 	cp -f $< $@
 
-$(distdir)/index.html: $(srcdir)/index.html
-	cp -f $< $@
+# $(distdir)/_chessboard.css: $(cmdir)/assets/chessboard.css
+# 	cp -f $< $@
+$(DISTDIR)/_ankicm.css: chessboard.css $(exts_css)
+	cat $^ > $@
 
-$(distdir)/_chessboard.css: $(cmdir)/assets/chessboard.css
-	cp -f $< $@
-
-
-$(chessdir)/chess.js: $(chessdir)/chess.ts
-	cd $(chessdir) && tsc 
+$(CHESSDIR)/chess.js: $(CHESSDIR)/chess.ts
+	cd $(CHESSDIR) && tsc 
 
 .PHONY: esbuild
 
-esbuild: $(nodedir)/esbuild
+esbuild: $(NODEDIR)/esbuild
 
-$(nodedir)/esbuild:
+$(NODEDIR)/esbuild:
 	npm install --save-exact --save-dev esbuild
-
 
 .PHONY: clean install
 
 clean:
-	rm -f $(targets) $(extra-targets)
+	rm -f $(TARGETS) $(EXTRA_TARGETS)
 
-install: $(targets) $(extra-targets)
-	cp -f $^ $(ankidir)
+install: $(TARGETS) $(EXTRA_TARGETS)
+	cp -f $^ $(INSTALLDIR)
 
---
+++ src/ankicm.js
@@ -0,0 +1,203 @@
+import { COLOR, Chessboard, BORDER_TYPE } from "../cm-chessboard/src/Chessboard";
+import { MARKER_TYPE, Markers } from "../cm-chessboard/src/extensions/markers/Markers.js";
+import {PROMOTION_DIALOG_RESULT_TYPE, PromotionDialog} from "../cm-chessboard/src/extensions/promotion-dialog/PromotionDialog.js";
+import {Accessibility} from "../cm-chessboard/src/extensions/accessibility/Accessibility.js";
+import {Chess} from "../chess.js/src/chess.js";
+
+
+function send_to_terminal(m) {
+    var myterminal=document.getElementById("myterminal");
+    var mymessage=document.createElement("div");
+    mymessage.appendChild(document.createTextNode(m));
+    myterminal.appendChild(mymessage);
+}
+
+(function(){
+    var oldError = console.error;
+    var oldWarn = console.warn;
+    var oldLog = console.log;
+
+    window.onerror = function(message, source, lineno, colno, error) {
+        send_to_terminal(message);
+    };
+
+    console.error = function(message) {
+        send_to_terminal(message);
+        oldError.apply(console, arguments);
+    }
+
+    console.warn = function(message) {
+        send_to_terminal(message);
+        oldWarn.apply(console, arguments);
+    }
+
+    console.log = function(message) {
+        send_to_terminal(message);
+        oldLog.apply(console, arguments);
+    }
+})();
+
+let boards = document.getElementsByClassName("cm-board");
+
+for (element of boards) {
+    createBoard(element);
+}
+
+
+function createBoard(element) {
+    var fen = element.textContent.trim();
+    // Remove FEN string from page.
+    element.innerHTML = "";
+
+    let board = new Chessboard(element, {
+        assetsUrl: "./",
+        position: fen,
+        orientation: boardOrientation(element, fen),
+        style: { pieces: { file: "_ankicm-standard.svg", } },
+        extensions: [
+            {class: Markers, props: {autoMarkers: MARKER_TYPE.square}},
+            {class: PromotionDialog},
+            {class: Accessibility, props: {visuallyHidden: true}}
+        ]
+    });
+}
+
+function boardOrientation(element, fen) {
+    let override = element.dataset.orientation || 'auto';
+
+    switch (override) {
+    case "white":
+        return COLOR.white;
+    case "black":
+        return COLOR.black;
+    case "auto":
+        const fenRegex = /(w|b) [kKqQ-]* [a-z0-9-] \d+ \d+$/;
+        let match = fen.match(fenRegex);
+
+        if (match[1] == "w") {
+            return COLOR.white;
+        } else if (match[1] == "b") {
+            return COLOR.black;
+        } else {
+            new Error("FEN does not specify whose turn it is in " + element);
+        }
+    default:
+        new Error ("data-orientation must be either 'white', 'black' or 'auto'");
+    }
+}
+
+
+
+    // import {INPUT_EVENT_TYPE, COLOR, Chessboard, BORDER_TYPE} from "../src/Chessboard.js"
+    // import {MARKER_TYPE, Markers} from "../src/extensions/markers/Markers.js"
+    // import {PROMOTION_DIALOG_RESULT_TYPE, PromotionDialog} from "../src/extensions/promotion-dialog/PromotionDialog.js"
+    // import {Accessibility} from "../src/extensions/accessibility/Accessibility.js"
+    // import {Chess} from "https://cdn.jsdelivr.net/npm/chess.mjs@1/src/chess.mjs/Chess.js"
+
+    const chess = new Chess()
+
+    // let seed = 71;
+    // function random() {
+    //     const x = Math.sin(seed++) * 10000;
+    //     return x - Math.floor(x);
+    // }
+    // function makeEngineMove(chessboard) {
+    //     const possibleMoves = chess.moves({verbose: true})
+    //     if (possibleMoves.length > 0) {
+    //         const randomIndex = Math.floor(random() * possibleMoves.length)
+    //         const randomMove = possibleMoves[randomIndex]
+    //         setTimeout(() => { // smoother with 500ms delay
+    //             chess.move({from: randomMove.from, to: randomMove.to})
+    //             chessboard.setPosition(chess.fen(), true)
+    //             chessboard.enableMoveInput(inputHandler, COLOR.white)
+    //         }, 500)
+    //     }
+    // }
+
+    function inputHandler(event) {
+        console.log("inputHandler", event)
+        if(event.type === INPUT_EVENT_TYPE.movingOverSquare) {
+            return // ignore this event
+        }
+        if(event.type !== INPUT_EVENT_TYPE.moveInputFinished) {
+            event.chessboard.removeMarkers(MARKER_TYPE.dot)
+            event.chessboard.removeMarkers(MARKER_TYPE.bevel)
+        }
+        if (event.type === INPUT_EVENT_TYPE.moveInputStarted) {
+            const moves = chess.moves({square: event.squareFrom, verbose: true})
+            for (const move of moves) { // draw dots on possible squares
+                if (move.promotion && move.promotion !== "q") {
+                    continue
+                }
+                if (event.chessboard.getPiece(move.to)) {
+                    event.chessboard.addMarker(MARKER_TYPE.bevel, move.to)
+                } else {
+                    event.chessboard.addMarker(MARKER_TYPE.dot, move.to)
+                }
+            }
+            return moves.length > 0
+        } else if (event.type === INPUT_EVENT_TYPE.validateMoveInput) {
+            const move = {from: event.squareFrom, to: event.squareTo, promotion: event.promotion}
+            const result = chess.move(move)
+            if (result) {
+                event.chessboard.state.moveInputProcess.then(() => { // wait for the move input process has finished
+                    event.chessboard.setPosition(chess.fen(), true).then(() => { // update position, maybe castled and wait for animation has finished
+                        makeEngineMove(event.chessboard)
+                    })
+                })
+            } else {
+                // promotion?
+                let possibleMoves = chess.moves({square: event.squareFrom, verbose: true})
+                for (const possibleMove of possibleMoves) {
+                    if (possibleMove.promotion && possibleMove.to === event.squareTo) {
+                        event.chessboard.showPromotionDialog(event.squareTo, COLOR.white, (result) => {
+                            console.log("promotion result", result)
+                            if (result.type === PROMOTION_DIALOG_RESULT_TYPE.pieceSelected) {
+                                chess.move({from: event.squareFrom, to: event.squareTo, promotion: result.piece.charAt(1)})
+                                event.chessboard.setPosition(chess.fen(), true)
+                                makeEngineMove(event.chessboard)
+                            } else {
+                                // promotion canceled
+                                event.chessboard.enableMoveInput(inputHandler, COLOR.white)
+                                event.chessboard.setPosition(chess.fen(), true)
+                            }
+                        })
+                        return true
+                    }
+                }
+            }
+            return result
+        } else if (event.type === INPUT_EVENT_TYPE.moveInputFinished) {
+            if(event.legalMove) {
+                event.chessboard.disableMoveInput()
+            }
+        }
+    }
+
+    const board = new Chessboard(document.getElementById("myboard"), {
+        position: chess.fen(),
+        assetsUrl: "../assets/",
+        style: {borderType: BORDER_TYPE.none, pieces: {file: "pieces/staunty.svg"}, animationDuration: 300},
+        orientation: COLOR.white,
+        extensions: [
+            {class: Markers, props: {autoMarkers: MARKER_TYPE.square}},
+            {class: PromotionDialog},
+            {class: Accessibility, props: {visuallyHidden: true}}
+        ]
+    })
+    board.enableMoveInput(inputHandler, COLOR.white)
+
+
+// var myboard = new Chessboard(document.getElementById("test"), {
+//     assetsUrl: "./",
+//     position: "rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR w Gkq - 4 11",
+//     pieces: { file: "standard.svg" }
+// })
+
+        // return this.chessboard.props.assetsUrl + this.chessboard.props.style.pieces.file;
+
+
+// new Chessboard(document.getElementById("board"), {
+//     assetsUrl: "./assets/",
+//     position: "rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR w Gkq - 4 11",
+// });
